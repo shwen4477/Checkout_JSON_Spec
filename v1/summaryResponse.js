5786ddfa394352fcca826563a6890a103cd5922c
Tim <tsmirnov@live.ru>
2016-10-20 17:59:04 +0300

    Create summaryResponse.js

---
+++ v1/summaryResponse.js
@@ -0,0 +1,96 @@
+{
+    "sets" :                   //Массив наборов товаров при разном количестве отправлений
+    [
+        {
+            "id" : int,
+            "date": string,
+            "price": decimal,       //ID сета
+            "items" :
+            [
+                {
+                    "id" : int,                     //ID товара
+                    "quantity" : int,             //Количество
+                    "price" : decimal,            //Цена
+                    "discountPrice" : int,        //Цена со скидкой, если таковая имеется
+                    "freeRest" : int,             //Сколько в хорошей доступности
+                    "totalRest" : int             //Сколько всего
+                }  
+            ]
+        }
+    ],
+    "deliveryTypes" :
+    [
+        {
+            "id": int,
+            "name": string,                         //ID Типа доставки (курьерская \ самовывоз \ почта россии)
+            "isSelected" : bool,              //Признак выбран ли тип доставки
+            "minPrice" : decimal,              //Минимальная по больнице минимальная стоимость доставки относительно типа
+            "maxPrice" : decimal,                 //Максимальная стоимость доставки относительно типа
+            "deliveryVariants":
+            [
+                {
+                    "id" : int,               //ID Способа доставки
+                    "isSelected" : bool,      //Признак выбран ли конкретный способ доставки
+                    "price" : decimal,        //Стоимость доставки
+                    "details":
+                    {
+                        "name" : string,
+                        "address": string,      //Адрес в случае ПВЗ
+                        "metro": string,        //Ближайшая станция метро в случае ПВЗ
+                        "schedule": string      //Расписание работы (а-ля пн-пт с 00:01 до 00:02 etc)
+                    } 
+
+                }
+            ],
+     ],
+     "paymentTypes" :                           //Типы оплаты
+     [
+         {
+             "id" : int,                        //ID типа оплаты
+             "name": string,                    //Название типа оплаты
+             "isSelected" : bool,               //Признак выбран ли тип
+             "paymentVariants" :
+             [
+                 {
+                     "id" : int,                            //ID способа оплаты                       
+                     "isSelected" : bool,               //Признак выбран ли способ
+                     "type" : ("online" | "offline"),      //Признак онлайн\оффлайн для группировки
+                     "details":
+                     {
+                         "name": string,  //Название способа оплаты
+                         "icon": string, //Ссылка на иконочку способа оплаты
+                         "description" : string, //Описашка для способа оплаты
+                         "hint" : string    //Юзерхинт для способа (есть кейсы, когда его нужно светить юзеру)
+                     } 
+
+                 }
+             ]
+          }
+      ],
+      "timeSlots" :                      //Тайм слоты на способ доставки
+      [
+          {
+              "id" : guid,               //ID слота
+              "isSelected" : bool,      //Признак выбран ли таймслот
+              "dateFrom" : datetime,        //От
+              "dateTo" : datetime,       //До
+              "timeLimit" : time,       //Cut off time
+           }
+       ],
+       "marketingActions" :                          //Сработавшие маркетинговые акции
+       [
+           {
+               "id" : int,                              //ID акции
+               "discountValue" : decimal,                //Сколько скинули
+               "type" : ("delivery" | "items"),      //Тип маркетинговой акции (на доставку, на товары)
+           }
+       ],      
+       "summary":
+        {
+          "weight": double, //Вес
+          "itemsPrice": double, //Цена за товары
+          "totalPrice" : double, //Цена за весь заказ
+          "availablePoints": decimal, //Сколько баллов можно потратить
+          "availableAccount" : decimal     //Сколько денег с ПС можно потратить
+        }
+}
