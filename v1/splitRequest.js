045525d4d75c0028277f0125646d08292ffcac1f
Mikhail <KindWizzard@users.noreply.github.com>
2016-11-02 16:46:25 +0300

    Create splitRequest.js

---
+++ v1/splitRequest.js
@@ -0,0 +1,10 @@
+{
+  "areaId": int, // id области доставки
+  "deliveryItems": [ // товары в корзине
+    {
+      "id": 0,
+      "quantity": 0
+    }
+  ],
+  "deliveryVariantId": int // способ доставки
+}
