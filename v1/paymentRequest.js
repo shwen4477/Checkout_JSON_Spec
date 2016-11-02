48832351ecc8d11b3d94120b38c9e748f7098fc2
Mikhail <KindWizzard@users.noreply.github.com>
2016-11-02 17:01:24 +0300

    Create paymentRequest.js

---
+++ v1/paymentRequest.js
@@ -0,0 +1,12 @@
+{
+  "items": [
+    {
+      "price": 0,
+      "quantity": 0
+    }
+  ],
+  "deliveryVariantId": 0,
+  "deliveryPrice": 0,       // зачем этот параметр? У нас же есть deliveryVariantId, на основе него и счиатайте
+  "sellerId": 0,            // зачем он нужен? можем ли мы убрать?
+  "areaId": 0
+}
